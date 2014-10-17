8edcbeee2d48ba2e5eea627e2a61cb0b1f0411ee
Grant Gingell <grantgingell@gmail.com>
2014-10-16 22:02:34 -0400

    Updated word-count to use a smarter regex, allows for skipping .trim() check for empty strings.

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,13 +1,9 @@
 var words = function(input) {
   var counts = {};
-  var words = input.split(/\s/);
+  var words = input.split(/\s+/);
 
   for(var i = words.length - 1; i >= 0; i--) {
     var w = words[i];
-    
-    if(w.trim() === '') {
-      continue; 
-    }
 
     if(counts.hasOwnProperty(w)) {
       counts[w]++;
